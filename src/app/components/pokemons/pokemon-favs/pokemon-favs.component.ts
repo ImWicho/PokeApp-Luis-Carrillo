d25478524e25a901734110ea158b04acc64ff993
Luis Carrillo <luis.carrillo.pereez@gmail.com>
2020-12-11 23:31:37 -0600

    [ADD] pokemons-fav view was added

--- src/app/components/pokemons/pokemon-favs/pokemon-favs.component.ts
+++ src/app/components/pokemons/pokemon-favs/pokemon-favs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-favs',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-favs.component.scss']
 })
 export class PokemonFavsComponent implements OnInit {
-
-  constructor() { }
+  pokemons: Pokemon[] = [];
+  constructor(private pokemonSvc: PokemonService) { }
 
   ngOnInit(): void {
+    this.pokemons = this.pokemonSvc.pokemons;
   }
 
 }
